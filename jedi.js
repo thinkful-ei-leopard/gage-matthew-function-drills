a8125a718a37242d4a370a26bc58336ad07c0646
AveraqeDev <averaqedev@gmail.com>
2020-01-06 12:38:32 -0800

    Added beyond function

--- jedi.js
+++ jedi.js
@@ -1,5 +1,22 @@
+/* eslint-disable strict */
 function createJediName(firstName, lastName) {
-    return lastName.substring(0, 3)+firstName.substring(0,2)
+  return lastName.substring(0, 3) + firstName.substring(0,2);
 }
 
 console.log(createJediName('Gage', 'Eide'));
+
+function beyond(number) {
+  if(!isFinite(number)) {
+    console.log('and Beyond!');
+  } else if(isFinite(number)) {
+    if(number > 0) {
+      console.log('To Infinity');
+    } else if(number < 0) {
+      console.log('To Negative Infinity');
+    } else {
+      console.log('Staying Home');
+    }
+  }
+}
+
+beyond('hello');
