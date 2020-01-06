cf8be3a5784066482829db7f99250e0570143a14
Gage Eide <gageeide@gmail.com>
2020-01-06 13:47:02 -0600

    added getYearOfBirth function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /* eslint-disable strict */
+function getYearOfBirth(age) {
+    return 2020 - age;
+}
+
 function createGreeting(name, age) {
-  const yearOfBirth = 2020 - age;
+  const yearOfBirth = getYearOfBirth();
   return `Hi, my name is ${name} and I'm ${age} years old! I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Gage', 25);
 console.log(greeting1);
