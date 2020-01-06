3ad46017840c4ea64e5cbfb6b84b4f19765a4fc4
AveraqeDev <averaqedev@gmail.com>
2020-01-06 11:44:14 -0800

    Added yearOfBirth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable strict */
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I\'m ${age} years old!`;
+  const yearOfBirth = 2020 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old! I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting();
