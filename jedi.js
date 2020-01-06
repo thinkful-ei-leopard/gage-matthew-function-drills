48b9e58ed719b8c4f9d01e91a49a118d3a35b641
AveraqeDev <averaqedev@gmail.com>
2020-01-06 13:10:16 -0800

    Added daysInMonth

--- jedi.js
+++ jedi.js
@@ -22,17 +22,70 @@ function beyond(number) {
 beyond('hello');
 
 function decode(word) {
-    if (word[0] === 'a') {
-        return word[1]
-    } else if (word[0] === 'b') {
-        return word[2]
-    } else if (word[0] === 'c') {
-        return word[3]
-    } else if (word[0] === 'd') {
-        return word[4]
-    } else {
-        return ' '
-    }
+  if (word[0] === 'a') {
+    return word[1];
+  } else if (word[0] === 'b') {
+    return word[2];
+  } else if (word[0] === 'c') {
+    return word[3];
+  } else if (word[0] === 'd') {
+    return word[4];
+  } else {
+    return ' ';
+  }
 }
 let words = ['craft', 'block', 'argon', 'meter', 'bells', 'brown', 'croon', 'droop']
 words.forEach((word) => console.log(decode(word)));
+
+function daysInAMonth(month, leapYear = false) {
+  let days = 0;
+  switch (month) {
+    case 'January':
+      days = 31;
+      break;
+    case 'February':
+      days = (leapYear ? 29 : 28);
+      break;
+    case 'March':
+      days = 31;
+      break;
+    case 'April':
+      days = 30;
+      break;
+    case 'May':
+      days = 31;
+      break;
+    case 'June':
+      days = 30;
+      break;
+    case 'July':
+      days = 31;
+      break;
+    case 'August':
+      days = 31;
+      break;
+    case 'September':
+      days = 30;
+      break;
+    case 'October':
+      days = 31;
+      break;
+    case 'November':
+      days = 30;
+      break;
+    case 'December':
+      days = 31;
+      break;
+    default:
+      throw new Error("Must provide a valid month!");
+      break;
+  }
+  return `${month} has ${days} days.`;
+}
+
+try {
+  console.log(daysInAMonth('Bob'));
+} catch (error) {
+  console.log(error);
+}
+
