97f212b006a3f4413e769dfd83b3422f0e5f60ed
AveraqeDev <averaqedev@gmail.com>
2020-01-06 12:14:08 -0800

    Added name and age typof Check

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function getYearOfBirth(age) {
 
 function createGreeting(name, age) {
   if (!name || !age) {
-      throw new Error('Name and Age need to be defined.')
+    throw new Error('Name and Age need to be defined.');
+  }
+  if(typeof name !== 'string' || typeof age !== 'number' || Number.isNaN(age)) {
+    throw new TypeError('Name must be a string and Age must be a number!');
   }
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old! I was born in ${yearOfBirth}.`;
